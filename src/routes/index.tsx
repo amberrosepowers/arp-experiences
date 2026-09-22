79ab9f6dd38221873610178cb4bfa5245c71fac8
amberrosepowers <amberrosepowers@gmail.com>
2026-09-21 20:15:49 -0500

    Add files via upload

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -76,9 +76,6 @@ function Home() {
         </div>
       </section>
 
-      {/* Cream spacer */}
-      <div className="h-10 md:h-16" />
-
       {/* Statement */}
       <section className="bg-walnut text-primary-foreground">
         <div className="mx-auto max-w-[1400px] px-6 py-16 text-center md:px-12 md:py-20">
